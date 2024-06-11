581223418971ff20c80405b89ae5768e3b55ad5e
Bijeesh M <bijeeshh.m@gmail.com>
2024-06-11 14:15:39 +0530

    fix:fixed the bug in styles

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -42,55 +42,61 @@ const Navbar = () => {
     };
 
     return (
-        <div
-            className={
-                show
-                    ? "block fixed top-0 w-full bg-black border-b border-b-violet-900 shadow-md shadow-violet-950 z-50"
-                    : "hidden"
-            }
-            // initial={{ opacity: 0 }}
-            // whileInView={{ opacity: 1 }}
-        >
-            <nav className="navbar bg-transparent sticky p-3  font-mono font-bold ">
-                <div className="flex-1">
-                    <a className="btn btn-ghost text-[24px]">Bijeesh</a>
-                </div>
-                <div className="flex-none ">
-                    <ul className="menu menu-horizontal px-1 text-[18px]">
-                        <li>
-                            <a
-                                href="#work"
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    scrollTo("work");
-                                }}
-                            >
-                                Projects
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="#about"
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    scrollTo("about");
-                                }}
-                            >
-                                About
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="#about"
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    scrollTo("contact");
-                                }}
-                            >
-                                Contact
-                            </a>
-                        </li>
-                    </ul>
+        // <motion.div
+        //     className={show? "block fixed top-0 w-full bg-black border-b border-b-violet-900 shadow-md shadow-violet-950 z-50": "hidden"}
+        //     initial={{ opacity: 0 }}
+        //     whileInView={{ opacity: 1 }}
+        // >
+        //     <nav className="navbar bg-transparent sticky p-3  font-mono font-bold ">
+        //         <div className="flex-1">
+        //             <a className="btn btn-ghost text-2xl">Bijeesh</a>
+        //         </div>
+        //         <div className="flex-none ">
+        //             <ul className="menu menu-horizontal px-1 text-xs sm:text-sm md:text-lg">
+        //                 <li>
+        //                     <a
+        //                         href="#work"
+        //                         onClick={(e) => {
+        //                             e.preventDefault();
+        //                             scrollTo("work");
+        //                         }}
+        //                     >
+        //                         Projects
+        //                     </a>
+        //                 </li>
+        //                 <li>
+        //                     <a
+        //                         href="#about"
+        //                         onClick={(e) => {
+        //                             e.preventDefault();
+        //                             scrollTo("about");
+        //                         }}
+        //                     >
+        //                         About
+        //                     </a>
+        //                 </li>
+        //                 <li>
+        //                     <a
+        //                         href="#about"
+        //                         onClick={(e) => {
+        //                             e.preventDefault();
+        //                             scrollTo("contact");
+        //                         }}
+        //                     >
+        //                         Contact
+        //                     </a>
+        //                 </li>
+        //             </ul>
+        //         </div>
+        //     </nav>
+        // </motion.div>
+        <div className=" text-white">
+            <nav className=" flex justify-between p-5 text-lg">
+                <div>Bijeesh</div>
+                <div className=" w-1/3 flex justify-between ">
+                    <p>Projects</p>
+                    <p>About</p>
+                    <p>Contact</p>
                 </div>
             </nav>
         </div>
