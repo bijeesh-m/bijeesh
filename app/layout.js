b6767109e9d005ea1e99ac0dc8a40ef14c195943
Bijeesh M <bijeeshh.m@gmail.com>
2024-06-11 14:37:50 +0530

    fix:fixed the bug in styles

--- app/layout.js
+++ app/layout.js
@@ -13,14 +13,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            {/* <head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <head>
+                {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap"
                     rel="stylesheet"
-                />
-            </head> */}
+                /> */}
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </head>
             <body className={inter.className}>
                 <Navbar />
                 {/* {children}
